d1732680d70299985463533fcdbbd666a5774fd8
Josh Amore <joshamore31@gmail.com>
2017-02-26 16:41:49 +1100

    Upload for use on GitHub Pages

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/*
+    This function pulls the quote from an API. 
+*/
+function getQuote() {
+    $.ajaxSetup({ cache: false });
+    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function(quoteResults) {
+        document.getElementById('message').innerHTML = quoteResults[0].content;
+        document.getElementById('author').innerHTML = "- " + quoteResults[0].title;
+        tweetQuote();
+        
+    });
+
+}
+
+function tweetQuote() {
+    var quote = $('#message').text() + $('#author').text();
+    quote = encodeURIComponent(quote);
+    
+    $("#tweetButton").html('<a target="_blank" href="https://twitter.com/intent/tweet?text=' + quote + '">Tweet</a>');  
+}
