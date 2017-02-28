8f209af8467d5e8687b7c073cb2b09277581e7fb
Josh Amore <joshamore31@gmail.com>
2017-02-28 20:10:29 +1100

    attempting to get api call function to work
    Call still not working. Made a second attempt, which also didn’t
    succeed.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-
-
 /*
     This function pulls the quote from an API. 
+    
+    // Below callback is not working (with &callback=? attached.).
 */
 function getQuote() {
     $.ajaxSetup({ cache: false });
-    $.getJSON('https://crossorigin.me/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', function(quoteResults) {
+    var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+    $.getJSON(url, null, function(quoteResults) {
         document.getElementById('message').innerHTML = quoteResults[0].content;
         document.getElementById('author').innerHTML = "- " + quoteResults[0].title;
         tweetQuote();
@@ -19,3 +20,24 @@ function tweetQuote() {
     
     $("#tweetButton").html('<a target="_blank" href="https://twitter.com/intent/tweet?text=' + quote + '"><img src="assets/twitter_icon.svg" id="twitterIcon"></i></a>');  
 }
+
+
+
+
+// Not working alt attempt at API request. 
+/*
+    var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        crossDomain: true,
+        headers: { 'Access-Control-Allow-Origin' : 'url' },
+        success: function (quoteResults) {
+            document.getElementById('message').innerHTML = quoteResults[0].content;
+            document.getElementById('author').innerHTML = "- " + quoteResults[0].title;
+            tweetQuote();
+
+        }
+    });
+*/
