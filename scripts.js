8d613921cb25ad84158c87900ab6dd59f5b41624
Josh Amore <joshamore31@gmail.com>
2017-03-01 18:46:07 +1100

    change to HTTPS API

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ function tweetQuote() {
 
 // This function pulls the quote from an API. 
 function getQuote() {    
-    var url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
+    var url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback";
     $.ajax({
         url: url,
         type: 'GET',
